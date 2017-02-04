f60e12713a5831207c75dd19c0d485d01ca639c9
Qiming Gu <cruiser31@gmail.com>
2017-02-04 17:32:49 +0800

    添加orders表

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -59,6 +59,38 @@ CREATE TABLE `merchants` (
 DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
 COMMENT='商户表';
 
+CREATE TABLE `event_msg` (
+  `event_msg_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id字段',
+  `to_user_name` varchar(32) DEFAULT NULL COMMENT '开发者微信号',
+  `from_user_name` varchar(32) DEFAULT NULL COMMENT '发送方帐号（一个OpenID）',
+  `create_time` timestamp NULL DEFAULT NULL COMMENT '消息创建时间',
+  `msg_type` varchar(10) DEFAULT NULL COMMENT '消息类型（地理消息为 event ）',
+  `event` varchar(32) DEFAULT NULL COMMENT '事件类型',
+  `msg_id` int(11) DEFAULT NULL COMMENT '消息id，64位整型',
+  `callback_status` varchar(16) DEFAULT NULL COMMENT '微信回复推送状态',
+  `push_status` varchar(16) DEFAULT NULL COMMENT '推送状态',
+  `order_id` int(11) DEFAULT NULL COMMENT '订单id',
+  PRIMARY KEY (`event_msg_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用于保存微信主动通知送达情况明细';
+
+CREATE TABLE `orders` (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `merchant_id` bigint(20) DEFAULT NULL COMMENT '商户号（沃智）',
+  `merchant_name` varchar(128) DEFAULT NULL COMMENT '商户名称',
+  `create_time` timestamp NULL DEFAULT NULL COMMENT '订单创建时间',
+  `txn_amt` decimal(16,2) DEFAULT NULL COMMENT '交易金额',
+  `currency_code` varchar(3) DEFAULT NULL COMMENT '货币种类（如CNY）',
+  `order_state` varchar(1) DEFAULT NULL COMMENT '订单状态',
+  `finish_time` timestamp NULL DEFAULT NULL COMMENT '订单完成时间',
+  `settle_amt` decimal(16,2) DEFAULT NULL COMMENT '清算金额',
+  `settle_date` timestamp NULL DEFAULT NULL COMMENT '清算日期',
+  `card_type` varchar(2) DEFAULT NULL COMMENT '卡类型',
+  `up_stream_id` bigint(20) DEFAULT NULL COMMENT '上游订单号',
+  PRIMARY KEY (`order_id`),
+  KEY `fk_merchant_id` (`merchant_id`),
+  CONSTRAINT `fk_merchant_id` FOREIGN KEY (`merchant_id`) REFERENCES `merchants` (`merchant_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单流水表';
+
 --插入初始数据
 INSERT INTO 
 	_user(user_name, user_phone, score)
