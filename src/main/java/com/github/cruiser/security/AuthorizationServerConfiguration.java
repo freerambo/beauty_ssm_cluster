134470cbeee7f0b5e68622518abdb79a8fb4dac9
Qiming Gu <cruiser31@gmail.com>
2017-03-27 00:02:34 +0800

    完善oauth验证

--- src/main/java/com/github/cruiser/security/AuthorizationServerConfiguration.java
+++ src/main/java/com/github/cruiser/security/AuthorizationServerConfiguration.java
@@ -1,7 +1,10 @@
 package com.github.cruiser.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -16,40 +19,64 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
-	private static String REALM="MY_OAUTH_REALM";
-	
-	@Autowired
-	private TokenStore tokenStore;
+    private static final Logger LOG = LoggerFactory.getLogger(AuthorizationServerConfiguration.class);
 
-	@Autowired
-	private UserApprovalHandler userApprovalHandler;
+    private static String REALM;
 
-	@Autowired
-	@Qualifier("authenticationManagerBean")
-	private AuthenticationManager authenticationManager;
+    @Value("${server.security.oauth2.client_id}")
+    private String CLIENT_ID;
 
-	@Override
-	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    //"my-trusted-client"
+    @Value("${server.security.oauth2.secret}")
+    private String SECRET;
 
-		clients.inMemory()
-	        .withClient("my-trusted-client")
-            .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
-            .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
-            .scopes("read", "write", "trust")
-            .secret("secret")
-            .accessTokenValiditySeconds(120).//Access token is only valid for 2 minutes.
-            refreshTokenValiditySeconds(600);//Refresh token is only valid for 10 minutes.
-	}
+    @Value("${server.security.oauth2.access_token_timeout}")
+    private int ACCESS_TOKEN_TIMEOUT;
 
-	@Override
-	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.tokenStore(tokenStore).userApprovalHandler(userApprovalHandler)
-				.authenticationManager(authenticationManager);
-	}
+    @Value("${server.security.oauth2.refresh_token_timeout}")
+    private int REFRESH_TOKEN_TIMEOUT;
 
-	@Override
-	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-		oauthServer.realm(REALM+"/client");
-	}
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Autowired
+    private UserApprovalHandler userApprovalHandler;
+
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
+
+
+    @Value("${server.security.oauth2.realm}")
+    public void setREALM(String REALM) {
+        LOG.debug("realm string is:" + REALM);
+        AuthorizationServerConfiguration.REALM = REALM;
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+        clients.inMemory()
+                .withClient(CLIENT_ID)
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+                .scopes("read", "write", "trust")
+                .secret(SECRET)
+                .accessTokenValiditySeconds(ACCESS_TOKEN_TIMEOUT).//Access token is only valid for 2 minutes.
+                refreshTokenValiditySeconds(REFRESH_TOKEN_TIMEOUT);//Refresh token is only valid for 10 minutes.
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore).userApprovalHandler(userApprovalHandler)
+                .authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        LOG.debug("realm string is:" + REALM);
+        oauthServer.realm(REALM);
+    }
 
 }
--- src/main/java/com/github/cruiser/security/MethodSecurityConfig.java
+++ src/main/java/com/github/cruiser/security/MethodSecurityConfig.java
@@ -7,13 +7,21 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
+/**
+ * @EnableGlobalMethodSecurity 可以指定 securedEnabled=true 也可以指定
+ * prePostEnabled=true，区别在于 前者只能够用于应用简单的基于角色的约束，
+ * 后者可以基于spring el 表达式来指定约束
+ * 可以参考：
+ *[spring-security-reference](https://vincentmi.gitbooks.io/spring-security-reference-zh/content/3.8_method_security.html)
+ *[Expression-Based Access Control](http://docs.spring.io/spring-security/site/docs/3.1.x/reference/el-access.html)
+ */
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
 public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
     @SuppressWarnings("unused")
-	@Autowired
+    @Autowired
     private OAuth2SecurityConfiguration securityConfig;
-    
+
     @Override
     protected MethodSecurityExpressionHandler createExpressionHandler() {
         return new OAuth2MethodSecurityExpressionHandler();
